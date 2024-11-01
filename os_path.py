396d3e82f5aef6a2fdf9e94aeda486a99e29ad3e
DND-N3RD-Al3x <darklord9142@gmail.com>
2024-11-01 12:06:57 -0700

    exported from Skill Struck

---
+++ os_path.py
@@ -0,0 +1,21 @@
+#os path: allows you to find, create, manage, delete, and maintain diffrent folders.
+
+#io path: allows you to read and write to files
+
+#module: another name for a single .py file
+
+#package: a folder cntaining multiple python modules
+
+#import: used to load other python source code files into current code page
+
+#os.path.isfile(): specifies wether a file exists or not in boolean
+
+#os.path.isdir(): specifies wether directory exists or not in boolean
+
+#os.path.remove(): removes a file frome directory.
+
+#os.path.basename(): retyrns the basename of a file
+
+#os.path.dirname(): gives the directory name where the folder or file is located
+
+#sys.path(): creates a list that contains all directories automaticaly when srarting a python file
