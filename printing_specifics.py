b3b9a75e00e8f492e1e5863ca01652e764ed53d7
DND-N3RD-Al3x <darklord9142@gmail.com>
2024-11-01 12:06:56 -0700

    exported from Skill Struck

---
+++ printing_specifics.py
@@ -0,0 +1,6 @@
+#print("I love \n to code!") = split 1 down
+#print("I love \n to /n code!") = split 2 down
+#print("I love \t to code!") = I love    to code
+#print("I love \r to code!") = to code!
+print("1\t\t\t\tSenior\n3\t\t\tJunior\n2\t\tSophmore\n1\tFreshman")
+print("Once \nUpon \na \nTime")
