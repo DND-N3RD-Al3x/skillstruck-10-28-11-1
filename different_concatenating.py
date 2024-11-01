e2b5bbdfc6774a62ece068abb80c0bfc48a8582d
DND-N3RD-Al3x <darklord9142@gmail.com>
2024-11-01 12:06:56 -0700

    exported from Skill Struck

---
+++ different_concatenating.py
@@ -0,0 +1,32 @@
+#Plus Symbol+ Approach:
+name = "James"
+dogs = 4
+location = "home"
+
+print ("My name is "+ name +" and I have "+str(dogs)+" dogs and live in "+location)
+#f-string Approach:
+name = "James"
+age = 16
+grade = 10
+
+print (f"My name is {name} and I have {dogs} dogs and live in {location}")
+
+#Comma Approach
+name = "James"
+age = 16
+grade = 10
+
+print ("My name is" , name , "and I have" ,dogs , "dogs and live in" , location)
+
+var1 = "a"
+var2 = "b"
+var3 = "c"
+var4 = "d"
+var5 = "e"
+int1 = 1
+int2 = 2
+int3 = 3
+int4 = 4
+int5 = 5
+print(int1, int2, int3, int4, int5,)
+
